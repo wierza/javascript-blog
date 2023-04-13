63eedfc7f9995c23489880c71511b7ef3a4ccb80
wierza <129267265+wierza@users.noreply.github.com>
2023-04-13 12:29:48 +0200

    Add articleSelector

--- js/script.js
+++ js/script.js
@@ -5,23 +5,48 @@
     console.log('links:', links);
   });*/
 
-  const titleClickHandler = function(event){
+const titleClickHandler = function(event){
+    event.preventDefault();
+    const clickedElement = this;
     console.log('Link was clicked!', event);
-    /* remove class 'active' from all article links  */
+    
+
+/* remove class 'active' from all article links  */
+
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+        for(let activeLink of activeLinks){
+            activeLink.classList.remove('active');
+        }
 
   /* add class 'active' to the clicked link */
 
+    clickedElement.classList.add('active');
+        console.log('clickedElement', clickedElement);
+
+
   /* remove class 'active' from all articles */
 
+    const activeArticles = document.querySelectorAll('.posts article.active');
+
+        for(let activeArticle of activeArticles){
+            activeArticle.classList.remove('active');
+
+         }
   /* get 'href' attribute from the clicked link */
+
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log(articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
+
+
   /* add class 'active' to the correct article */
-  }
+}
   
-  const links = document.querySelectorAll('.titles a');
+const links = document.querySelectorAll('.titles a');
   
-  for(let link of links){
+    for(let link of links){
     link.addEventListener('click', titleClickHandler);
-  }
+}
