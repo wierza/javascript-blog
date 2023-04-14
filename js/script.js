6c783897de4773fdfb1f9049149b2f3a12011ba7
wierza <129267265+wierza@users.noreply.github.com>
2023-04-14 10:03:08 +0200

    Add targetArticle

--- js/script.js
+++ js/script.js
@@ -36,11 +36,12 @@ const titleClickHandler = function(event){
   /* get 'href' attribute from the clicked link */
 
     const articleSelector = clickedElement.getAttribute('href');
-    console.log(articleSelector);
+    console.log('kliknięty artykuł to', articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
-
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
 
   /* add class 'active' to the correct article */
 }
