eaadbd85829b59ae4e97ccce518423987a04671d
wierza <129267265+wierza@users.noreply.github.com>
2023-04-18 16:40:18 +0200

    delete tags' list

--- js/script.js
+++ js/script.js
@@ -48,7 +48,8 @@ const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles',
   optArticleTagsSelector = '.post-tags .list',
-  optAuthorSelector = '.post-author';
+  optAuthorSelector = '.post-author',
+  optTagsListSelector = '.tags.list';
 
 function generateTitleLinks(customSelector = ''){
   console.log(customSelector);
@@ -96,6 +97,10 @@ generateTitleLinks();
 
 
 function generateTags(){
+
+  /* [NEW] create a new variable allTags with an empty array */
+  let allTags = [];
+
   /* find all articles */
 
   const articles = document.querySelectorAll(optArticleSelector);
@@ -136,14 +141,32 @@ function generateTags(){
       html = html + linkHTML;
       console.log(html);
 
+      /* [NEW] check if this link is NOT already in allTags */
+
+      if(allTags.indexOf(linkHTML) == -1){
+
+        /* [NEW] add generated code to allTags array */
+
+        allTags.push(linkHTML);
+      }
+
       /* END LOOP: for each tag */
     }
     /* insert HTML of all the links into the tags wrapper */
 
     tagList.innerHTML = html;
 
-  /* END LOOP: for every article: */
+    /* END LOOP: for every article: */
+
   }
+
+  /* [NEW] find list of tags in right column */
+
+  const tagList = document.querySelector(optTagsListSelector);
+
+  /* [NEW] add html from allTags to tagList */
+
+  tagList.innerHTML = allTags.join(' ');
 }
 generateTags();
 
@@ -342,3 +365,4 @@ function addClickListenersToAuthors(){
   }
 }
 addClickListenersToAuthors();
+
